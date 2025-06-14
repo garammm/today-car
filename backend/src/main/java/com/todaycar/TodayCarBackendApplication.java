4b1cf0b11822bbf753cb1fdd5be9923d5598f293
Garam Yu <garam010201@naver.com>
2025-06-14 19:01:19 +0900

    chore: DB 이름 수정

--- backend/src/main/java/com/todaycar/TodayCarBackendApplication.java
+++ backend/src/main/java/com/todaycar/TodayCarBackendApplication.java
@@ -8,17 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.todaycar.board.repository")
 @EntityScan(basePackages = "com.todaycar.board.domain")
 public class TodayCarBackendApplication {
 
-    // @PostConstruct
-    // public void startH2Server() throws SQLException {
-    //     Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092").start();
-    // }
 
     public static void main(String[] args) {
         SpringApplication.run(TodayCarBackendApplication.class, args);
